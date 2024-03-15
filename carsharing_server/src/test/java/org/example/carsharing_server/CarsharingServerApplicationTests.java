db52dc3e92c1328df4b76ada2cea3b76b56e58bb
Lili Nemes <lili.nemes@outlook.com>
2024-03-15 18:16:08 +0100

    Setting up Spring project

---
+++ carsharing_server/src/test/java/org/example/carsharing_server/CarsharingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.carsharing_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsharingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
